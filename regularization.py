ea2a2dd7241def0847d6b1172e297e1edfdcf181
ting <thetingzhang@gmail.com>
2018-08-17 21:02:14 -0400

    implement l1 penalty

--- regularization.py
+++ regularization.py
@@ -3,13 +3,41 @@ import numpy as np
 
 def l2(W, alpha=1.0):
     '''
-    calculate the l2 penalty for the weights W
+    calculate the l2 penalty (used in ridge regression) for the input weights W
 
-    where l2 penalty is equal to:
+    this is for educational purposes to compare l1 and l2 penalty on different weights
+
+    we expect to see l2 to produce a lower penalty when all values are minimized
+
+    the l2 penalty formula is:
 
     alpha * sum(W**2)
     '''
     return alpha * (W**2).sum()
 
 
-print(l2(np.array([1, 1, 2]), 2.0))
+def l1(W, alpha=1.0):
+    '''
+    calculate the l1 penalty (used in lasso regression) for the input weights W
+
+    this is for educational purposes to compare l1 and l2 penalty on different weights
+
+    we expect to see l1 to produce a lower penalty when some values are zero
+
+    the l1 penalty formula is:
+
+    alpha * sum(abs(W))
+    '''
+    return alpha * abs(W).sum()
+
+
+arr_1 = np.array([100] * 100)
+arr_2 = np.array([10000] + [0] * 99)
+
+print(l2(arr_1, 1.0))
+print(l2(arr_2, 1.0))
+print('')
+print(l1(arr_1, 1.0))
+print(l1(arr_2, 1.0))
+
+
