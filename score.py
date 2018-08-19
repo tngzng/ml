4a510ec700b7bb1a051b42575de27a78b8837964
ting <thetingzhang@gmail.com>
2018-08-19 13:14:31 -0400

    empty score files

---
+++ score.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def accuracy(y_actual, y_predicted):
+    '''
+    the fraction of samples that were classified correctly
+    '''
+    pass
+
+
+def recall(y_actual, y_predicted):
+    pass
+
+
+def precision(y_actual, y_predicted):
+    pass
