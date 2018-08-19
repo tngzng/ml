b58a4a63897f0a756be6903efff9285c36f7a841
ting <thetingzhang@gmail.com>
2018-08-19 13:37:41 -0400

    cute docstrings

--- score.py
+++ score.py
@@ -9,8 +9,32 @@ def accuracy(y_actual, y_predicted):
 
 
 def recall(y_actual, y_predicted):
+    '''
+    true positive rate
+
+    correctly predicted positive / all actual positive
+    '''
     pass
 
 
 def precision(y_actual, y_predicted):
+    '''
+    positive prediction rate
+
+    correctly predicted positive / all predicted positive
+    '''
+    pass
+
+
+def f_score(y_actual, y_predicted, beta=1.0):
+    '''
+    combine precision and recall into a single score, using beta to vary the weight of
+    precision or recall
+
+    a beta less than one will emphasize precision
+
+    a beta greater than one will emphasize recall
+
+    (1 + beta) * ((precision * recall) / ((beta * precision) + recall))
+    '''
     pass
